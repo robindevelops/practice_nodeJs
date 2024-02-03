08720d6d3de69ab997e9f549eb84e3a02d81051c
Alyan Shahid <alyanshahidsdk@gmail.com>
2024-02-03 20:16:56 +0500

    Require and Exports Done

---
+++ 1-Globals.js
@@ -0,0 +1,15 @@
+/* Golbals
+
+__dirname = file to current directory
+
+__filname = filname
+ 
+require  = file to use modules
+
+module = encapsulated code
+
+commonJs = every file is module
+
+
+
+*/
---
+++ app.js
@@ -0,0 +1,4 @@
+
+const names = require('./2-names.js')
+
+console.log(names.john)
